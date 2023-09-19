31322d2c37562410f280d562e3a467fb8701a8c5
kemasghani <kemasghani123@gmail.com>
2023-09-19 16:58:17 +0700

    fix bug

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Tilt from 'react-tilt';
 import { motion } from 'framer-motion';
 import { styles } from '../styles';
 import { services } from '../constants';
@@ -8,7 +7,7 @@ import { SectionsWrapper } from '../HOC';
 
 const ServiceCard = ({ title, icon, index }) => {
   return (
-    <Tilt className="xs:w-[250px] w-full">
+    <div className="xs:w-[250px] w-full">
       <motion.div
         variants={fadeIn('right', 'spring', 0.5 * index, 0.75)}
         className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
@@ -23,7 +22,7 @@ const ServiceCard = ({ title, icon, index }) => {
           </h3>
         </div>
       </motion.div>
-    </Tilt>
+    </div>
   );
 };
 
