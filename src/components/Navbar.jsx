3efb0494b90400e2c998db2e99c734cdb04f210f
Kemas Ghani <87101184+kemasghani@users.noreply.github.com>
2024-08-30 01:49:48 +0700

    remove nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,12 @@ import { Link } from 'react-router-dom';
 import { styles } from '../styles';
 import { navLinks } from '../constants';
 import { menu, close } from '../assets';
-import logoGhani from '../assets/logoGhani.png'
+import logoGhani from '../assets/logoGhani.png';
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
   const [active, setActive] = useState('');
+
   return (
     <div>
       <nav
@@ -27,52 +28,52 @@ const Navbar = () => {
               Kemas Ghani Sampurna
             </p>
           </Link>
-          <ul className="list-none hidden sm:flex flex-row gap-10">
-            {navLinks?.map((link) => {
-              return (
+         {/* { <ul className="list-none hidden sm:flex flex-row gap-10">
+            {navLinks?.map((link) => (
+              <li
+                key={link?.id}
+                className={`${active === link?.title ? 'text-white' : 'text-secondary'
+                  } text-medium hover:text-white text-[18px] cursor-pointer`}
+                onClick={() => setActive(link?.title)}
+              >
+                <Link to={`/#${link.id}`}>
+                  {link?.title}
+                </Link>
+              </li>
+            ))}
+          </ul>} */}
+          {/* For Mobile Navigation */}
+        {/* <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img
+            src={toggle ? close : menu}
+            className="w-[28px] h-[28px] object-contain cursor-pointer"
+            onClick={() => {
+              setToggle(!toggle);
+            }}
+          />
+          <div
+            className={`${!toggle ? 'hidden' : 'flex'
+              } p-6 black-gradient absolute top-20 right-0 mx-x my-2 min-w-[180px] z-10 rounded-xl`}
+          >
+            <ul className="list-none flex flex-col justify-end items-start gap-4">
+              {navLinks?.map((link) => (
                 <li
                   key={link?.id}
                   className={`${active === link?.title ? 'text-white' : 'text-secondary'
-                    } text-medium hover:text-white text-[18px] cursor-pointer`}
-                  onClick={() => setActive(link?.title)}
+                    } text-medium text-[16px] font-poppins cursor-pointer`}
+                  onClick={() => {
+                    setActive(link?.title);
+                    setToggle(!toggle);
+                  }}
                 >
-                  <a href={`#${link?.id}`}>{link?.title}</a>
+                  <Link to={`/#${link.id}`}>
+                    {link?.title}
+                  </Link>
                 </li>
-              );
-            })}
-          </ul>
-          {/* // For Mobile Navigation */}
-          <div className="sm:hidden flex flex-1 justify-end items-center">
-            <img
-              src={toggle ? close : menu}
-              className="w-[28px] h-[28px] object-contain cursor-pointer"
-              onClick={() => {
-                setToggle(!toggle);
-              }}
-            />
-            <div
-              className={`${!toggle ? 'hidden' : 'flex'
-                } p-6 black-gradient absolute top-20 right-0 mx-x my-2 min-w-[180px] z-10 rounded-xl`}
-            >
-              <ul className="list-none flex flex-col justify-end items-start  gap-4">
-                {navLinks?.map((link) => {
-                  return (
-                    <li
-                      key={link?.id}
-                      className={`${active === link?.title ? 'text-white' : 'text-secondary'
-                        } text-medium text-[16px] font-poppins cursor-pointer`}
-                      onClick={() => {
-                        setActive(link?.title);
-                        setToggle(!toggle);
-                      }}
-                    >
-                      <a href={`#${link?.id}`}>{link?.title}</a>
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
+              ))}
+            </ul>
           </div>
+        </div> */}
         </div>
       </nav>
     </div>
